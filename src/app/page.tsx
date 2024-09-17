690dfc57b61167e124a73a0f1a3e952665d0194a
unknown <fabioflauzino@hotmail.com>
2024-09-16 23:53:15 -0300

    Iniciando criação da página de feed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import { redirect } from "next/navigation";
+import { Logo } from "../components/ui/logo";
+
 export default function Home() {
+  redirect("/home")
+
   return (
-    <div>...</div>
+    <div className="min-h-screen flex justify-center items-center">
+      <Logo size={80} />
+    </div>
   );
 }

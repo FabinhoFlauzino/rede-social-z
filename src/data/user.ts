ec1e7a67ced50c77c7cb6fa7b811cc0673b93e70
Fabio Flauzino <fabio@mfrural.com.br>
2024-09-18 14:59:08 -0300

    Criação do componente editar perfil

--- src/data/user.ts
+++ src/data/user.ts
@@ -6,5 +6,6 @@ export const user: User = {
   avatar: 'https://github.com/FabinhoFlauzino.png',
   cover: 'https://images.unsplash.com/photo-1504353888070-4ee702492320?q=80&w=2037&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   link: 'https://github.com/FabinhoFlauzino',
-  postCount: 123
+  postCount: 123,
+  bio: "Desenvolvedor a mais de 3 anos"
 }
